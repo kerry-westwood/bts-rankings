b024b14f49b5b208ea05983cc779b62049e24092
kerry-westwood <kerry0887@gmail.com>
2015-11-29 17:26:20 +0000

    Deleting connections

--- scripts/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = "localhost";
-$user = "shotstat";
-$pwd = "2ADfZm7P:76,Wy96";
-$dbn = "richhemi_shotstat";
-
-mysql_connect($server, $user, $pwd) or die(mysql_error());
-mysql_select_db($dbn) or die(mysql_error());
-?>
