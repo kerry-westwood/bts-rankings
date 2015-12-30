6b3509dc13b9a7a56381780132e9d3d809a85710
Richard-Hemingway <richardhemingway@Richards-MacBook.local>
2015-12-30 21:14:39 +0000

    Competitor Listing Script
    Scripts to Fetch Competitor Details and check which disciplines they
    have rankings in.

--- query/competitor_listing.php
+++ query/competitor_listing.php
@@ -1,10 +1,43 @@
 <?php
 include '../scripts/conn.php';
 
-$compsql="SELECT compID, surname, forename, nationality FROM competitor ORDER BY surname";
+/* Select Competitor Details and checks which Disciplines they have results for. */
+
+$compsql=	"SELECT competitorID, surname, forename, nationality, gender,
+		IFNULL((SELECT scorepr.competitorID
+         FROM scorepr
+         WHERE competitor.competitorID = scorepr.competitorID
+         ORDER BY scorepr.scoreprID DESC
+         LIMIT 1
+       ),0) AS prone, 
+       IFNULL((SELECT score3p.competitorID
+         FROM score3p
+         WHERE competitor.competitorID = score3p.competitorID
+         ORDER BY score3p.score3pID DESC
+         LIMIT 1
+       ),0) AS 3p, 
+       IFNULL((SELECT scorear.competitorID
+         FROM scorear
+         WHERE competitor.competitorID = scorear.competitorID
+         ORDER BY scorear.scorearID DESC
+         LIMIT 1
+       ),0) AS ar, 
+       IFNULL((SELECT scoreap.competitorID
+         FROM scoreap
+         WHERE competitor.competitorID = scoreap.competitorID
+         ORDER BY scoreap.scoreapID DESC
+         LIMIT 1
+       ),0) AS ap
+FROM competitor
+ORDER BY surname";
+
 $compresult=mysql_query($compsql);
 
-$compoptions="";
+/* Clears $compoptions */
+
+/* $compoptions=""; */
+
+/* Outputs to php array, inserts values to $compoptions to populate drop-down in form */
 
 while ($row=mysql_fetch_array($compresult)) {
 
@@ -12,11 +45,20 @@ while ($row=mysql_fetch_array($compresult)) {
     $compsur=$row["surname"];
     $compfor=$row["forename"];
     $compnat=$row["nationality"];
+    $compgen=$row["gender"];
+    $comppr=$row["prone"];
+    $comp3p=$row["3p"];
+    $compar=$row["ar"];
+    $compap=$row["ap"];
     $compname=$compsur.", ".$compfor." (".$compnat.")";
-    $compoptions.="<OPTION VALUE=\"$compid\">".$compname;
+    /* $compoptions.="<OPTION VALUE=\"$compid\">".$compname; */
 	}
 ?>
 
+
+
+
+<!--
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -65,6 +107,4 @@ while ($row=mysql_fetch_array($compresult)) {
 
 </body>
 
-</html>
-
-
+</html> -->
